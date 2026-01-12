ce826dfbed7d029823012a2c56e5cbf32df49494
tregz00 <tmukomb1@stu.vistula.du.pl>
2026-01-12 20:00:28 +0100

    first project java spring

---
+++ src/main/java/pl/edu/vistula/first_project_java_spring/controller/HelloController.java
@@ -0,0 +1,26 @@
+package pl.edu.vistula.first_project_java_spring.controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.ui.Model;
+
+@Controller
+public class HelloController {
+
+    @GetMapping(value = "/")
+    public String hello(){
+        return "Hello Vistula in my first Spring controller";
+    }
+
+    @GetMapping("/greeting")
+    public String greeting(@RequestParam(name="name", required=false,defaultValue="World") String name,Model model){
+
+        model.addAttribute("name", name);
+        return "greeting";
+    }
+
+    //localhost:8080/greeting?name=Vistula
+
+
+
+}
